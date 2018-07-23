3b0a5c1e58cb734a4ea129af9da23d5b425d3865
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-23 09:21:36 +0700

    change code of mysql.php and mysqlii.php files

--- src/include/mysql.php
+++ src/include/mysql.php
@@ -5,11 +5,11 @@ if (!defined('INAPP')) {
 }
 
 class MySql {
-	private $queryCount = 0;	
-	private $conn = null;	
-	private $result = null;	
-	private $log_file = 'sql_log.sql';	
-	private static $instance = null;	
+	private $queryCount = 0;
+	private $conn = null;
+	private $result = null;
+	private $log_file = 'sql_log.sql';
+	private static $instance = null;
 	private function __construct() {
 		if (!function_exists('mysql_connect')) {
 			app_exit(array(
@@ -36,7 +36,7 @@ class MySql {
 				'data' => null,
 				'msg' => 'No database selected.'
 		));
-	}	
+	}
 	public static function getInstance() {
 		if (self::$instance == null) {
 			self::$instance = new MySql();
@@ -45,13 +45,12 @@ class MySql {
 	}
 	function close() {
 		return mysql_close($this->conn);
-	}	
+	}
 	private function sqllog($sql, $error = ''){
 		$fp = fopen(SITE_ROOT . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . $this->log_file, 'a');
-		fwrite($fp, "[".date('Y-m-d H:i:s')."]: ".$sql."\
-".(!empty($error) ? $error ."\n" : ""));
+		fwrite($fp, "[".date('Y-m-d H:i:s')."]: ".$sql."\n".(!empty($error) ? $error ."\n" : ""));
 		fclose($fp);
-	}	
+	}
 	function query($sql) {
 		$this->result = @mysql_query($sql, $this->conn);
 		if (defined('DEBUG_SQL')) {
@@ -67,41 +66,41 @@ class MySql {
 			));
 		}
 		return $this->result;
-	}	
+	}
 	function fetch_array($query , $type = MYSQL_ASSOC) {
 		return mysql_fetch_array($query, $type);
 	}
 	function once_fetch_array($sql) {
 		$this->result = $this->query($sql);
 		return $this->fetch_array($this->result);
-	}	
+	}
 	function fetch_row($query) {
 		return mysql_fetch_row($query);
-	}	
+	}
 	function num_rows($query) {
 		return mysql_num_rows($query);
-	}	
+	}
 	function num_fields($query) {
 		return mysql_num_fields($query);
-	}	
+	}
 	function insert_id() {
 		return mysql_insert_id($this->conn);
-	}	
+	}
 	function geterror() {
 		return mysql_error();
-	}	
+	}
 	function geterrno() {
 		return mysql_errno();
-	}	
+	}
 	function affected_rows() {
 		return mysql_affected_rows();
-	}	
+	}
 	function getMysqlVersion() {
 		return mysql_get_server_info();
-	}	
+	}
 	function getQueryCount() {
 		return $this->queryCount;
-	}	
+	}
 	function escape_string($sql) {
 		return mysql_real_escape_string($sql);
 	}
--- src/include/mysqlii.php
+++ src/include/mysqlii.php
@@ -5,11 +5,11 @@ if (!defined('INAPP')) {
 }
 
 class MySqlii {
-	private $queryCount = 0;	
-	private $conn = null;	
-	private $result = null;	
-	private $log_file = 'sql_log.sql';	
-	private static $instance = null;	
+	private $queryCount = 0;
+	private $conn = null;
+	private $result = null;
+	private $log_file = 'sql_log.sql';
+	private static $instance = null;
 	private function __construct() {
 		if (!class_exists('mysqli')) {
 			app_exit(array(
@@ -34,7 +34,7 @@ class MySqlii {
 			));
 		}
 		$this->conn->set_charset('utf8');
-	}	
+	}
 	public static function getInstance() {
 		if (self::$instance == null) {
 			self::$instance = new MySqlii();
@@ -43,12 +43,12 @@ class MySqlii {
 	}
 	function close() {
 		return $this->conn->close();
-	}	
+	}
 	private function sqllog($sql, $error = ''){
 		$fp = fopen(SITE_ROOT . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . $this->log_file, 'a');
 		fwrite($fp, "[".date('Y-m-d H:i:s')."]: ".$sql."\n".(!empty($error) ? $error ."\n" : ""));
 		fclose($fp);
-	}	
+	}
 	function query($sql) {
 		$this->result = $this->conn->query($sql);
 		if (defined('DEBUG_SQL')) {
@@ -64,41 +64,41 @@ class MySqlii {
 			));
 		}
 		return $this->result;
-	}	
+	}
 	function fetch_array(mysqli_result $query , $type = MYSQLI_ASSOC) {
 		return $query->fetch_array($type);
 	}
 	function once_fetch_array($sql) {
 		$this->result = $this->query($sql);
 		return $this->fetch_array($this->result);
-	}	
+	}
 	function fetch_row(mysqli_result $query) {
 		return $query->fetch_row();
-	}	
+	}
 	function num_rows(mysqli_result $query) {
 		return $query->num_rows;
-	}	
+	}
 	function num_fields(mysqli_result $query) {
 		return $query->field_count;
-	}	
+	}
 	function insert_id() {
 		return $this->conn->insert_id;
-	}	
+	}
 	function geterror() {
 		return $this->conn->error;
-	}	
+	}
 	function geterrno() {
 		return $this->conn->errno;
-	}	
+	}
 	function affected_rows() {
 		return $this->conn->affected_rows;
-	}	
+	}
 	function getMysqlVersion() {
 		return $this->conn->server_info;
-	}	
+	}
 	function getQueryCount() {
 		return $this->queryCount;
-	}	
+	}
 	function escape_string($sql) {
 		return $this->conn->real_escape_string($sql);
 	}
