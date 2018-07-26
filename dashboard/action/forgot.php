9e354eeadd4f2851aadbe584d8bbd11504bf221e
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-26 15:10:53 +0700

    update forgot.php change error code constan

--- dashboard/action/forgot.php
+++ dashboard/action/forgot.php
@@ -13,28 +13,48 @@ if (Auth::isLogged()) {
 if (isset($_POST['field_chk'])) {
 	$field_chk = trim($_POST['field_chk']);
 	if (empty($field_chk)) {
-		mDirect(Url::action('forgot',array('msg' => 5)));
+		access_response(array(
+			'status' => 'error',
+			'url' => Url::action('forgot', array('msg' => User::ERROR_INVALID)),
+			'msg' => User::message(User::ERROR_INVALID)
+		));
 		exit;
 	}
 	if (!is_mail($field_chk)) {
 		$user_data = Auth::getUserDataByUsername($field_chk);
 		if ($user_data === false) {
-			mDirect(Url::action('forgot',array('msg' => 1)));
+			access_response(array(
+				'status' => 'error',
+				'url' => Url::action('forgot', array('msg' => User::ERROR_NAME_NOTEXISTS)),
+				'msg' => User::message(User::ERROR_NAME_NOTEXISTS)
+			));
 			exit;
 		}
 		// Send Mail $user_data['email']
-		
-		mDirect(Url::home(array('msg' => 6)));
+
+		access_response(array(
+			'status' => 'success',
+			'url' => Url::action('forgot', array('msg' => User::ERROR_FORGOTED)),
+			'msg' => User::message(User::ERROR_FORGOTED)
+		));
 		exit;
 	} else {
 		$user_data = Auth::getUserDataByEmail($field_chk);
 		if ($user_data === false) {
-			mDirect(Url::action('forgot',array('msg' => 2)));
+			access_response(array(
+				'status' => 'error',
+				'url' => Url::action('forgot', array('msg' => User::ERROR_EMAIL_NOTEXISTS)),
+				'msg' => User::message(User::ERROR_EMAIL_NOTEXISTS)
+			));
 			exit;
 		}
 		// Send Mail  $user_data['email']
-		exit;
-		mDirect(Url::home(array('msg' => 6)));
+
+		access_response(array(
+                        'status' => 'success',
+                        'url' => Url::action('forgot', array('msg' => User::ERROR_FORGOTED)),
+                        'msg' => User::message(User::ERROR_FORGOTED)
+                ));
 		exit;
 	}
 }
