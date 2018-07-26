04ad8bd9b303e9d47c5704d89589db58bf55c65b
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-26 15:18:12 +0700

    update profile.php source code

--- dashboard/action/profile.php
+++ dashboard/action/profile.php
@@ -12,21 +12,21 @@ if (isset($_POST['saved'])) {
 	);
 	$hasError = false;
 	if (!is_mail($u_data['email'])) {
-		$_GET['msg'] = 1;
+		$_GET['msg'] = User::ERROR_EMAIL_INVALID;
 		$hasError = $hasError || true;
 	}
 	if (empty($u_data['nickname'])) {
-		$_GET['msg'] = 2;
+		$_GET['msg'] = User::ERROR_FULLNAME_NONE;
 		$hasError = $hasError || true;
 	}
 	if ($UModel->is_email_exists($u_data['email'], Auth::get('id', -1))) {
-		$_GET['msg'] = 3;
+		$_GET['msg'] = User::ERROR_EMAIL_EXISTS;
 		$hasError = $hasError || true;
 	}
 	if (isset($_POST['password']) && !empty($_POST['password'])) {
 		$pwd = trim($_POST['password']);
 		if (strlen($pwd) < 5) {
-			$_GET['msg'] = 4;
+			$_GET['msg'] = User::ERROR_PASSWORD_INVALID;
 			$hasError = $hasError || true;
 		} else {
 			$PHPASS = new PasswordHash(8, true);
@@ -36,10 +36,14 @@ if (isset($_POST['saved'])) {
 	if ($hasError == false) {
 		$id = $UModel->update($u_data, intval(Auth::get('id',-1)));
 		if ($id > 0) {
-			mDirect(Url::profile(array('msg' => 5)));
+			access_response(array(
+				'status' => 'success',
+				'url' => Url::action('profile', array('msg' => User::ERROR_UPDATED)),
+				'msg' => User::message(User::ERROR_SAVED)
+			));
 			exit;
 		} else {
-			$_GET['msg'] = 6;
+			$_GET['msg'] = User::ERROR_UPDATE;
 		}
 	}
 }
