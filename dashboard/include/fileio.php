468149d8e81fb2a9bee7a41fbcea920b6eef6375
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-27 14:50:10 +0700

    define instance auto create current month and year struct

--- dashboard/include/fileio.php
+++ dashboard/include/fileio.php
@@ -4,6 +4,15 @@ if (!defined('INAPP')) {
 	exit;
 }
 class FileIO {
+	public static function getInstance( $subdir = 'files' ) {
+		$currentDir = UPLOADPATH . DIRECTORY_SEPARATOR . $subdir  . DIRECTORY_SEPARATOR . str_replace('/', DIRECTORY_SEPARATOR, date('Y/m'));
+		if (!is_dir($currentDir)) {
+			@mkdir($currentDir, 0777, true);
+		}
+		if (is_dir($currentDir)) {
+			define('CURRENT_DIR_UPLOAD', $currentDir);
+		}
+	}
 	public static function upload() {}
 }
 ?>
