418f97b2237dc1705667e3fbf2447379e70e7cd2
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-27 09:12:48 +0700

    Update line error from ' character to ; character

--- dashboard/model/user.php
+++ dashboard/model/user.php
@@ -5,7 +5,7 @@ if (!defined('INAPP')) {
 }
 class User {
 	const ERROR_NAME_NOTEXIST = 1;
-	const ERROR_EMAIL_NOEXISTS = 2'
+	const ERROR_EMAIL_NOEXISTS = 2;
 	const ERROR_LOCKED = 3;
 	const ERROR_INVALID = 4;
 	const ERROR_NAME_EXISTS = 5;
