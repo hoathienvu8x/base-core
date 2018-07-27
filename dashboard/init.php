33a9291bed6e9b7179bd3adb9556624d54a6e166
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-27 14:30:33 +0700

    define upload path and upload url to init.php file

--- dashboard/init.php
+++ dashboard/init.php
@@ -16,6 +16,9 @@ define('SITE_URL',Option::get('site_url','http://localhost/dashboard/'));
 define('TEMPLATEPATH', SITE_ROOT . '/template/');
 define('TEMPLATEURL', SITE_URL . 'template/');
 
+define('UPLOADPATH', SITE_ROOT . DIRECTORY_SEPARATOR . 'uploads');
+define('UPLOADURL', SITE_URL . '/uploads/');
+
 if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 	define('AJAX_DOING', true);
 }
