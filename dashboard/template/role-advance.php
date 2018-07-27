c1e4f75bf4cf838480c22b719736ae53b4007665
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-27 05:45:44 +0000

    update role-advance fill data

--- dashboard/template/role-advance.php
+++ dashboard/template/role-advance.php
@@ -7,22 +7,25 @@ if (!defined('INAPP')) {
 require_once TEMPLATEPATH . 'header.php';
 ?>
 <div class="form-elements">
-	<form action="<?php echo Url::action('role'); ?>" method="post">
+	<form action="<?php echo Url::role(); ?>" method="post">
 		<p>
 			Role name<br />
-			<input type="text" name="role_name" value="" placeholder="Role name" />
+			<input type="text" name="role_name" value="<?php echo isset($role['role_name']) ? $role['role_name'] : ''; ?>" placeholder="Role name" />
 		</p>
 		<?php if (Auth::is_supper()) : ?>
 		<p>
 			Role alias<br />
-			<input type="text" name="role_alias" value="" placeholder="Role alias" />
+			<input type="text" name="role_alias" value="<?php echo isset($role['role_alias']) ? $role['role_alias'] : ''; ?>" placeholder="Role alias" />
 		</p>
 		<?php endif; ?>
 		<p>
 			Description<br />
-			<textarea name="role_desc" placeholder="Description" />
+			<textarea name="role_desc" placeholder="Description"><?php echo isset($role['role_desc']) ? $role['role_desc'] : ''; ?></textarea>
 		</p>
 		<p class="bottom">
+			<?php if (isset($role['id']) && intval($role['id']) > 0) : ?>
+			<input type="hidden" name="id" value="<?php echo $role['id']; ?>" />
+			<?php endif; ?>
 			<input type="submit" name="saved" value="Lưu thông tin" />
 		</p>
 	</form>
