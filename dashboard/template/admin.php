411fb5005a683eefc0805bad4dfce1b4109691e1
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-30 16:41:35 +0700

    List admin template

--- dashboard/template/admin.php
+++ dashboard/template/admin.php
@@ -5,12 +5,37 @@ if (!defined('INAPP')) {
 }
 
 require_once TEMPLATEPATH . 'header.php';
+if (isset($_GET['msg'])) {
+        $msg = User::message(intval($_GET['msg']));
+        $msg_status = in_array(intval($_GET['msg']),array(User::ERROR_SAVED, User::ERROR_UPDATED, User::ERROR_DELETED, User::ERROR_CHANGED)) ? ' success' : '';
+        echo '<div class="error_notify'.$msg_status.'">'.$msg.'</div>';
+}
 ?>
-
+<table>
+<thead>
+<tr>
+	<th><input type="checkbox" name="chkAll" value="1" /></th>
+	<th><a href="<?php echo Url::admin(); ?>">NickName</a></th>
+	<th><a href="<?php echo Url::admin(); ?>">Username</a></th>
+	<th>Email</th>
+	<th>&nbsp;</th>
+</tr>
+</thead>
+<tbody>
 <?php foreach($users as $row) : ?>
-
+<tr>
+	<td><input type="checkbox" name="checked[]" value="<?php echo $row['id']; ?>" /></td>
+	<td><strong><?php echo $row['nickname']; ?></strong></td>
+	<td><?php echo $row['username'];?></td>
+	<td><?php echo $row['email']; ?></td>
+	<td>
+		<a href="<?php echo Url::admin(array('edit' => $row['id'])); ?>">Edit</a>
+		<a href="<?php echo Url::admin(array('del' => $row['id'])); ?>">Delete</a>
+	</td>
+</tr>
 <?php endforeach; ?>
-
+</tbody>
+</table>
 <?php
 require_once TEMPLATEPATH . 'footer.php';
 exit;
