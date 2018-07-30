3e4839e34805fe938b2f82952a42fdc6b3a691d5
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 00:36:35 +0700

    Add auth access

--- dashboard/ajax.php
+++ dashboard/ajax.php
@@ -4,6 +4,9 @@ require_once dirname ( __FILE__ ) . '/init.php';
 
 
 header('Content-Type:application/json;charset=utf-8');
+
+Access::auth(); // Restrict process acction
+
 access_response(array(
 	'status' => 'error',
 	'url' => SITE_URL,
