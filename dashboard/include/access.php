dffb9d92d9f30f96f181a7bbe956d2f27a3cf556
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 00:39:14 +0700

    Update access response message

--- dashboard/include/access.php
+++ dashboard/include/access.php
@@ -8,8 +8,8 @@ class Access {
 		if (!Auth::isLogged()) {
 			access_response(array(
 				'status' => 'error',
-				'direct' => Url::home(),
-				'msg' => Auth::message()
+				'direct' => Url::home(array('msg' => Auth::ERROR_MUST_BE_LOGIN)),
+				'msg' => Auth::message(Auth::ERROR_MUST_BE_LOGIN)
 			));
 		}
 		$mode = isset($_GET['mode']) && !empty($_GET['mode']) ? trim($_GET['mode']) : '';
