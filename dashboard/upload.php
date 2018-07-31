9c81137422fae35818bbc770c78d2c73ab9db501
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 11:10:13 +0700

    Add support method and define bytes chunk length

--- dashboard/upload.php
+++ dashboard/upload.php
@@ -1,9 +1,23 @@
 <?php
 define ( 'INAPP', true );
 require_once dirname ( __FILE__ ) . '/init.php';
+define('BYTES_PER_CHUNK', 1048576);
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: x-requested-with, x-index, x-total, Content-Type, origin,accept");
 
 header('Content-Type:application/json;charset=utf-8');
+
+if (!defined('AJAX_DOING')) {
+	access_response(array(
+		'status' => 'error',
+		'url' => SITE_URL,
+		'msg' => 'Tải tập tin chỉ yêu cầu phương thức POST và gửi dữ liệu dưới dạng AJAX'
+	));
+	exit;
+}
 
 if (!isset($_POST['action']) && !preg_match('/^(avatar|file)$/i',strtolower($_POST['action']))) {
 	access_response(array(
