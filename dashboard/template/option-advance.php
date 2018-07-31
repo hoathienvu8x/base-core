16c03be232c20f29ebbfd2fb40afc259b24118bb
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 10:39:04 +0700

    Change role to supper add option name

--- dashboard/template/option-advance.php
+++ dashboard/template/option-advance.php
@@ -7,17 +7,22 @@ if (!defined('INAPP')) {
 require_once TEMPLATEPATH . 'header.php';
 if (isset($_GET['msg'])) {
 	$code = intval($_GET['msg']);
-	$msg = Option::message(intval($code);
+	$msg = Option::message($code);
 	$msg_status = in_array($code, array(Option::ERROR_SAVED, Option::ERROR_UPDATED, Option::ERROR_DELETED, Option::ERROR_LOADED)) ? ' success' : '';
 	echo '<div class="error_notify'.$msg_status.'">'.$msg.'</div>';
+}
+if (empty($args)) {
+	$args['mode'] = 'add';
 }
 ?>
 <div class="form-elements">
 	<form action="<?php echo Url::option($args); ?>" method="post">
+		<?php if (!isset($args['edit']) && Auth::is_super()) : ?>
 		<p>
 			Option name<br />
 			<input type="text" name="option_name"<?php echo isset($args['option_name']) ? ' readonly="readonly"' : ''; ?> value="<?php echo isset($option['option_name']) ? $option['option_name'] : ''; ?>" placeholder="Role name" />
 		</p>
+		<?php endif; ?>
 		<p>
 			Option value<br />
 			<textarea name="option_value" placeholder="Option value"><?php echo isset($option['option_value']) ? $option['option_value'] : ''; ?></textarea>
