da0613eefe38104e686dda31bbf8533f4e4a839a
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 13:57:46 +0700

    List item of options

--- dashboard/template/option.php
+++ dashboard/template/option.php
@@ -6,11 +6,27 @@ if (!defined('INAPP')) {
 
 require_once Xtemplate::get( 'header' );
 ?>
+<table>
+<thead>
 
+</thead>
+<tbody>
 <?php foreach($options as $row) : ?>
-
+<tr>
+	<?php $is_system = Option::is_system($row['option_name']); ?>
+	<td><input type="checkbox" name="checked[]"<?php echo $is_system ? ' disable="disable"' : ''; ?> value="<?php echo $row['option_name'];?>" /></td>
+	<td><?php echo $row['option_desc']; ?></td>
+	<td><?php echo Option::option_value($row); ?></td>
+	<td>
+		<a href="<?php echo Url::option(array('edit' => $row['option_name'])); ?>">Edit</a>
+		<?php if ($is_system == false) : ?>
+		<a href="<?php echo Url::option(array('del' => $row['option_name'])); ?>">Delete</a>
+		<?php endif; ?>
+	</td>
+</tr>
 <?php endforeach; ?>
-
+</tbody>
+</table>
 <?php
 require_once Xtemplate::get( 'footer' );
 exit;
