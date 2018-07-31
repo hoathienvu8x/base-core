a5094b5f2f488bd17ffe5b83b15c6b918ff19bd7
Tran Ngoc Nhat <anhchangthode22145@gmail.com>
2018-07-31 23:37:37 +0700

    Add admin template

---
+++ admin/admin-advance.php
@@ -0,0 +1,29 @@
+<?php require_once 'header.php'; ?>
+<form action="#" method="post">
+	<p>
+		Label<br />
+		<input type="text" name="a" value="" placeholder="" />
+	</p>
+	<p>
+		label<br />
+		<textarea name="b" placeholder=""></textarea>
+	</p>
+	<p>
+		Label<br />
+		<select name="c">
+			<option value="">Option</option>
+		</select>
+	</p>
+	<p>
+		<label for=""><input type="radio" value="" id="" />Label</label>
+		<label for=""><input type="checkbox" value="" id="" />Label</label>
+	</p>
+	<p>
+		Label<br />
+		<input type="file" name=""  />
+	</p>
+	<p>
+		<input type="submit" name="" value="Submit" />
+	</p>
+</form>
+<?php require_once 'footer.php'; ?>
