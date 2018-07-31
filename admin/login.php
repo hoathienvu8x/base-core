3112fe39eddb1cd3d840c7dd23558aed2918b11e
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-01 00:38:10 +0700

    update login template

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,18 @@
 <?php require_once 'header.php'; ?>
-
+<form action="" method="post">
+	<div id="login-form">
+		<p>
+			Username:<br />
+			<input type="text" name="username" value="" placeholder="" />
+		</p>
+		<p>
+			Password<br />
+			<input type="password" name="password" placeholder="" />
+		</p>
+		<p class="bottom">
+			<a href="<?php echo Url::forgot(); ?>">Forgot password ?</a>
+			<input type="submit" name="logon" value="Login" />
+		</div>
+	</div>
+</form>
 <?php require_once 'footer.php'; ?>
