d02d2d3643ae86a22127b8d5b8e820def5ec040b
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 12:22:43 +0700

    Add load script in footer

--- dashboard/template/footer.php
+++ dashboard/template/footer.php
@@ -7,5 +7,6 @@ if (!defined('INAPP')) {
 <div class="admin-footer<?php echo Auth::isLogged() ? ' is-logged' : '';?>">&copy; 2018<?php echo date('Y') > 2018 ? '-'.date('y') : '';?> AnhChangThoDe22145 All Rights Reserved</div>
 <?php if( Auth::isLogged() ) : ?>
 </div>
+<script src="<?php echo TEMPLATEURL;?>js/script.js"></script>
 <?php endif; ?>
 </body></html>
