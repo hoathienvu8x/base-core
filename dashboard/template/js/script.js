e10be1d6a3ef738c5b14763ccfeba41ad0b38d8e
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 09:11:09 +0700

    add utf82assci, trim, ucFirst and make_alias to execute in client

--- dashboard/template/js/script.js
+++ dashboard/template/js/script.js
@@ -1,3 +1,36 @@
+String.prototype.toUcFirst = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+};
+String.prototype.trim = function() {
+        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+        return this.replace(rtrim, "").replace(/^\s+|\s+$/g,"");
+};
+function ut8f2assci(str) {
+	str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+	str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+	str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+	str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+	str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+	str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+	str = str.replace(/đ/g, "d");
+	str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+	str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+	str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+	str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+	str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+	str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+	str = str.replace(/Đ/g, "D");
+	str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+	str = str.replace(/ + /g," ");
+	return str.trim();
+}
+function make_alias(str, c) {
+	c = c || '-';
+	str = ut8f2assci(str).toLowerCase();
+	str = str.replace(/ {2,}/g,' ').trim();
+	str = str.replace(/ /g, c);
+	return str;
+}
 (function($) {
 	'use strict';
 	$.browser = $.browser || (function() {
