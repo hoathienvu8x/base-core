97bc7bec25e94b12945dbf8d31b0bc9803101e8d
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-07-31 09:58:14 +0700

    Update action and process data to save or update to database

--- dashboard/action/option.php
+++ dashboard/action/option.php
@@ -5,11 +5,14 @@ if (!defined('INAPP')) {
 }
 $pageTitle = 'ACP :: Quản lý quyền cho người dùng';
 
+$args = array();
+
 if (isset($_POST['saved'])) {
 	$data = array(
 		'option_name' => '',
 		'option_value' => '',
-		'option_desc' => ''
+		'option_desc' => '',
+		'autoload' => isset($_POST['autoload']) ? trim($_POST['autoload']) : 'n'
 	);
 	if (!isset($_POST['option_name']) || empty($_POST['option_name'])) {
 		access_response(array(
@@ -26,94 +29,110 @@ if (isset($_POST['saved'])) {
 	if (isset($_POST['option_desc'])) {
 		$data['option_desc'] = trim($_POST['option_desc']);
 	}
-	if (isset($_POST['id']) && intval($_POST['id']) > 0) {
-		if ($RModel->is_role_exists($data['role_alias'], intval($_POST['id']))) {
-			access_response(array(
-				'status' => 'error',
-				'direct' => Url::role(array('msg' => Role::ERROR_EXISTS)),
-				'msg' => Role::message(Role::ERROR_EXISTS)
-			));
-			exit;
-		}
-		$role_id = $RModel->update($data, intval($_POST['id']));
-		if ($role_id > 0) {
-			access_response(array(
-				'status' => 'sucess',
-				'direct' => Url::role(array('msg' => Role::ERROR_UPDATED)),
-				'msg' => Role::message(Role::ERROR_UPDATED)
-			));
-			exit;
+	if (isset($_POST['edit']) && !empty($_POST['edit'])) {
+		$option_name = trim($_POST['edit']);
+		if ($option_name == $data['option_name']) {
+			$retVal = Option::update($option_name, $data['option_value'], $data['option_desc'], $data['autoload']);
+			if ($retVal == true) {
+				$args['msg'] = Option::ERROR_UPDATED;
+				access_response(array(
+					'status' => 'success',
+					'direct' => Url::option($args),
+					'msg' => Option::message(Option::ERROR_UPDATED)
+				));
+				exit;
+			} else {
+				$args['msg'] = Option::ERROR_UPDATE;
+				access_response(array(
+					'status' => 'error',
+					'direct' => Url::option($args),
+					'msg' => Option::message(Option::ERROR_UPDATE)
+				));
+				exit;
+			}
 		} else {
+			$args['msg'] = Option::ERROR_UPDATE;
 			access_response(array(
 				'status' => 'error',
-				'direct' => Url::role(array('msg' => Role::ERROR_UPDATE)),
-				'msg' => Role::message(Role::ERROR_UPDATE)
+				'direct' => Url::option($args),
+				'msg' => Option::message(Option::ERROR_UPDATE)
 			));
 			exit;
 		}
 	} else {
-		if ($RModel->is_role_exists($data['role_alias'], null)) {
+		$option = Option::getOne($data['option_name']);
+		if (!$option) {
+			$args['msg'] = Option::ERROR_NOTEXISTS;
 			access_response(array(
-                                'status' => 'error',
-                                'direct' => Url::role(array('msg' => Role::ERROR_EXISTS)),
-                                'msg' => Role::message(Role::ERROR_EXISTS)
-                        ));
-                        exit;
-		}
-		$role_id = $RModel->add($data);
-		if ($role_id > 0) {
-			access_response(array(
-				'status' => 'success',
-				'direct' => Url::role(array('msg' => Role::ERROR_SAVED)),
-				'msg' => Role::message(Role::ERROR_SAVED)
+				'status' => 'error',
+				'direct' => Url::option($args),
+				'msg' => Option::message(Option::ERROR_NOTEXISTS)
 			));
 			exit;
 		} else {
-			access_response(array(
-				'status' => 'error',
-				'direct' => Url::role(array('msg' => Role::ERROR_SAVE)),
-				'msg' => Role::message(Role::ERROR_SAVE)
-			));
-			exit;
+			$retVal = Option::add($data['option_name'], $data['option_value'], $data['option_desc'], $data['autoload']);
+			if ($retVal == true) {
+				$args['msg'] = Option::ERROR_SAVED;
+				access_response(array(
+					'status' => 'success',
+					'direct' => Url::option($args),
+					'msg' => Option::message(Option::ERROR_SAVED)
+				));
+				exit;
+			} else {
+				$args['msg'] = Option::ERROR_SAVE;
+				access_response(array(
+					'status' => 'error',
+					'direct' => Url::option($args),
+					'msg' => Option::message(Option::ERROR_SAVE)
+				));
+				exit;
+			}
 		}
 	}
+
 }
 
 if (isset($_GET['edit']) && intval($_GET['edit']) > 0) {
-	$role = $RModel->getOne(intval($_GET['edit']));
-	if (!$role) {
+	$option = Option::getOne(trim($_GET['edit']));
+	$args['edit'] = trim($_GET['edit']);
+	if (!$option) {
+		$args['msg'] = Option::ERROR_NOTEXISTS;
 		access_response(array(
 			'status' => 'error',
-			'direct' => Url::role(array('msg' => Role::ERROR_NOTEXISTS)),
-			'msg' => Role::message(Role::ERROR_NOTEXISTS)
+			'direct' => Url::option($args),
+			'msg' => Option::message(Option::ERROR_NOTEXISTS)
 		));
 		exit;
 	}
-	$pageTitle = 'ACP :: Cập nhật quyền';
-	require_once TEMPLATEPATH . 'role-advance.php';
+	$pageTitle = 'ACP :: Cập nhật tuỳ chọn';
+	require_once TEMPLATEPATH . 'option-advance.php';
 	exit;
 }
 
 if (isset($_GET['mode']) && strtolower($_GET['mode']) == 'add') {
-	$pageTitle = 'ACP :: Thêm quyền';
-	require_once TEMPLATEPATH . 'role-advance.php';
+	$pageTitle = 'ACP :: Thêm tuỳ chọn';
+	$args['mode'] = 'add';
+	require_once TEMPLATEPATH . 'option-advance.php';
 	exit;
 }
 
 if (isset($_GET['del']) && intval($_GET['del']) > 0) {
-	$role_id = $RModel->delete(intval($_GET['del']));
-	if ($role_id > 0) {
+	$retVal = Option::delete(trim($_GET['del']));
+	if ($retVal == true) {
+		$args['msg'] = Option::ERROR_DELETED;
 		access_response(array(
 			'status' => 'success',
-			'dicrect' => Url::role(array('msg' => Role::ERROR_DELETED)),
-			'msg' => Role::message(Role::ERROR_DELETED)
+			'dicrect' => Url::option($args),
+			'msg' => Option::message(Option::ERROR_DELETED)
 		));
 		exit;
 	} else {
+		$args['msg'] = Option::ERROR_DELETE;
 		access_response(array(
 			'status' => 'error',
-			'direct' => Url::role(array('msg' => Role::ERROR_DELETE)),
-			'msg' => Role::message(Role::ERROR_DELETE)
+			'direct' => Url::option($args),
+			'msg' => Option::message(Option::ERROR_DELETE)
 		));
 		exit;
 	}
@@ -130,11 +149,7 @@ if (isset($_GET['byname']) && preg_match('/^(asc|desc)$/i', trim($_GET['byname']
 	$options['name'] = strtolower(trim($_GET['byname']));
 }
 
-if (isset($_GET['role']) && !empty(trim($_GET['role']))) {
-	$options['role'] = trim($_GET['role']);
-}
-
-$roles = $RModel->get($options, $page, Option::get('row_per_page', 15));
+$roles = Option::getAll($options, $page, Option::get('row_per_page', 15));
 
 require_once TEMPLATEPATH . 'role.php';
 exit;
