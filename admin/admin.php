330ea5e25087c24def9d5b8dd44e60cd452df5c7
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-01 14:03:12 +0700

    add page navigation and style that

--- admin/admin.php
+++ admin/admin.php
@@ -1,11 +1,20 @@
 <?php require_once 'header.php'; ?>
+<div class="navTools pageNavi">
+	<p>
+                <span class="total">1,123 mục</span>
+                <a class="first-page" href="#">Đầu tiên</a>
+                <span class="current"><input type="number" value="2" class="navi-page" name="page" />/120</span>
+                <a class="next-page" href="#">Trang sau</a>
+                <a class="last-page" href="#">Trang cuối</a>
+        </p>
+</div>
 <div class="data-table">
 	<table class="list-table">
 		<thead>
 			<tr>
 				<th class="checkbox"><input type="checkbox" name="chkAll" value="1" /></th>
-				<th>Nickname</th>
-				<th>Username</th>
+				<th><a href="<?php echo Url::admin(array('orderby' => 'name'));?>">Nickname</a></th>
+				<th><a href="<?php echo Url::admin(array('orderby' => 'username')); ?>">Username</a></th>
 				<th>Role</th>
 				<th class="tools">&nbsp;</th>
 			</tr>
@@ -24,4 +33,13 @@
 		</tbody>
 	</table>
 </div> <!-- End of data table -->
+<div class="navTools pageNavi">
+	<p>
+		<span class="total">1,123 mục</span>
+		<a class="first-page" href="#">Đầu tiên</a>
+		<span class="current"><input type="number" value="2" class="navi-page" name="page" />/120</span>
+		<a class="next-page" href="#">Trang sau</a>
+		<a class="last-page" href="#">Trang cuối</a>
+	</p>
+</div>
 <?php require_once 'footer.php'; ?>
